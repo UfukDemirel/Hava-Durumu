cf0fb6aa253f426931384ebb5136142575bd85cc
UfukDemirel <85198386+UfukDemirel@users.noreply.github.com>
2022-06-13 12:11:37 +0300

    Hava Durumu
    Laravel ile hava durumu API

---
+++ havadurumu/resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hava Durumu</title>
+    <link rel="stylesheet" href="/style.css">
+</head>
+<body>
+<div class="app">
+<div class="header">
+    <h1>Hava Durumu</h1>
+    <input type="text" id="searchBar" placeholder="Şehir Giriniz..">
+</div>
+    <div class="content">
+        <div class="city"> ,TR</div>
+        <div class="temp">0°C</div>
+        <div class="desc">Durum</div>
+        <div class="minmax">0°/0°</div>
+    </div>
+</div>
+<script src="/script.js"></script>
+</body>
+</html>
